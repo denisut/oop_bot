22e706b79a14dbb3dd48495e65a6570eae109b4e
ПК <borovkova112004@gmail.com>
2023-09-27 10:32:40 +0500

    z

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,14 +7,15 @@ public class Main {
 
         while(true) {
             String command = input.nextLine();
-            String s = switch(command) {
-                case "/help" -> "Бот для парсинга статистики с YouTube и VK \nДоступные команды: \n/YouTube \n/vk \n /exit";
-                case "/YouTube" -> "Отправьте ссылку на видео YouTube";
-                case "/VK" -> "Отправьте ссылку на пост VK";
-                case "/exit" -> throw new IllegalArgumentException("Выход");
-                default -> "Список доступных команд - /help";
+            switch(command) {
+                case "/help" -> System.out.println("Бот для парсинга статистики с YouTube и VK \nДоступные команды: \n/YouTube \n/vk \n /exit");
+                case "/YouTube" -> System.out.println("Отправьте ссылку на видео YouTube");
+                case "/VK" -> System.out.println("Отправьте ссылку на пост VK");
+                case "/exit" -> {
+                    System.exit(0);
+                }
+                default -> System.out.println("Список доступных команд - /help");
             };
-            System.out.println(s);
         }
 
     }
